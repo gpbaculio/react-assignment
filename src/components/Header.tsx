092f59a5677b4a4c8a6cadde3efd29717bdc3764
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-04-13 05:54:35 +0800

    add condition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 
+import { useAppSelector } from "../hooks";
+import { selectUser } from "../store/userSlice";
+
 function Header() {
+  const user = useAppSelector(selectUser);
+
   return (
     <header className="bg-blue-500 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -19,6 +24,25 @@ function Header() {
                 Home
               </Link>
             </li>
+            {!user ? (
+              <li>
+                <Link
+                  to="/login"
+                  className="hover:underline text-white hover:text-gray-200"
+                >
+                  Login
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <button
+                  // onClick={logout}
+                  className="text-white hover:text-gray-200 focus:outline-none"
+                >
+                  Logout
+                </button>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
