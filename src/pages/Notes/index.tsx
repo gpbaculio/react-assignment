cb11fe37e3e1ea3cc725e48c8f3b965a806ad2b8
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-04-13 08:10:40 +0800

    Update index.tsx

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,9 +1,11 @@
 import AddNoteForm from "./AddNoteForm";
+import NoteList from "./NoteList";
 
 function Notes() {
   return (
     <div className="container mx-auto px-4">
       <AddNoteForm />
+      <NoteList />
     </div>
   );
 }
