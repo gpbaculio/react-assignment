ffd0a572e1dfcd83cd35236011e59b6058537d2a
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-04-13 08:58:26 +0800

    Update Note.tsx

--- src/pages/Notes/Note.tsx
+++ src/pages/Notes/Note.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
 import { Note as NoteType } from "../../store/notesSlice";
 
 interface NoteProps {
